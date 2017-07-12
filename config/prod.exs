a2f9d8626647c976dddf11cb36f0c5fd379c4745
Jake Runzer <jakerunzer@gmail.com>
2017-07-12 00:37:11 -0700

    my god fix config again

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :elm_phoenix, ElmPhoenix.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :elm_phoenix, HelloPhoenix.Repo,
+config :elm_phoenix, ElmPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
