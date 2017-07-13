f3a3eb77bf1bdddf47d615e019a7e614717c1685
Jake Runzer <jakerunzer@gmail.com>
2017-07-12 22:11:07 -0700

    use imports again'

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,22 +1,16 @@
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-// import 'phoenix_html';
-
 // Import styles
-// import 'tachyons/css/tachyons.css';
+import 'tachyons/css/tachyons.css';
 import '../scss/app.scss';
+
+// Import dependencies
+import randomColour from 'randomcolor';
+console.log(randomColour());
 
 // import socket from './socket';
 
 // Elm
 
-const randomColour = require('randomcolor');
-// import randomColour from 'randomcolor';
-console.log(randomColour());
-
-const Elm = require('../../elm/Main.elm');
+import Elm from '../../elm/Main.elm';
 
 const elmDiv = document.getElementById('elm-main');
 
